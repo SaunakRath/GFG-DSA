a87067ed35fe87431b57368086412c72486d92c4
Saunak Rath <98891210+SaunakRath@users.noreply.github.com>
2024-12-08 14:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (22.32%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Check if the current element is greater than the next element
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        // Array is sorted and rotated if there is at most one "break" in the order
+        return count <= 1;
+    }
+}
